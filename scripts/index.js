fe714dfc354d0ee2b6981547402a6d3f75a58239
EricTran <traneric94@gmail.com>
2017-09-21 17:58:16 -0700

    add counter

--- scripts/index.js
+++ scripts/index.js
@@ -1,58 +1,67 @@
 function setup(){
-	for(var i=0;i<8;i++){
-		newWord("red");
-		newWord("blue");
-	}
-	for(var i=0;i<7;i++){
-		newWord("yellow");
-	}
-	newWord("black");
-	newWord().addClass("wildcard");
+    for(var i=0;i<8;i++){
+        newWord("red");
+        newWord("blue");
+    }
+    for(var i=0;i<7;i++){
+        newWord("yellow");
+    }
+    newWord("black");
+    newWord().addClass("wildcard");
 
-	$(".word").click(function(){
-		$(this).toggleClass("clicked");
-	});
+
+    $(".word").click(function(){
+        $(this).toggleClass("clicked");
+        count();
+    });
 
 }
 
 function newWord(color){
-	var index = $("#board").children().length;
-	return $("<div/>").attr("data-index",index).addClass("word").attr("data-color",color).append("<div/>").appendTo("#board");
+    var index = $("#board").children().length;
+    return $("<div/>").attr("data-index",index).addClass("word").attr("data-color",color).append("<div/>").appendTo("#board");
 }
 
 function fire(){
-	$("*").removeClass("spymaster clicked");
+    $("*").removeClass("spymaster clicked");
 
-	//get seed and set the seed for randomizer
-	Math.seedrandom($("#seed").val());
+    //get seed and set the seed for randomizer
+    Math.seedrandom($("#seed").val());
 
-	var wordSet = $("#undercover").prop('checked') ? undercover : data;
+    var wordSet = $("#undercover").prop('checked') ? undercover : data;
 
-	wordSet.sort();
+    wordSet.sort();
 
-	var color = Math.random() > 0.5 ? "red" : "blue";
-	$(".wildcard").attr("data-color",color);
-	$("#team").css({color:color}).text(color.toUpperCase());
+    var color = Math.random() > 0.5 ? "red" : "blue";
+    $(".wildcard").attr("data-color",color);
+    $("#team").css({color:color}).text(color.toUpperCase());
 
-	words = $(".word").sort(function(i,j){return i.getAttribute("data-index")-j.getAttribute("data-index")});
+    words = $(".word").sort(function(i,j){return i.getAttribute("data-index")-j.getAttribute("data-index")});
 
-	while(words.length > 0){
-		$(words.splice(Math.random()*words.length,1)[0]).appendTo("#board");
-	}
+    while(words.length > 0){
+        $(words.splice(Math.random()*words.length,1)[0]).appendTo("#board");
+    }
 
-	$(".word *").each(function(index,element){
-		if($("#seed").val()=="ericisgay"){element.innerHTML = Math.random()<0.33 ? "eric" : (Math.random()<0.5 ? "is" : "gay");return;};
-		var location = index+Math.floor(Math.random()*(wordSet.length-index));
-		element.innerHTML = wordSet[location];
-		wordSet[location] = wordSet[index];
-		wordSet[index] = element.innerHTML;
-	})
+    $(".word *").each(function(index,element){
+        if($("#seed").val()=="ericisgay"){element.innerHTML = Math.random()<0.33 ? "eric" : (Math.random()<0.5 ? "is" : "gay");return;};
+        var location = index+Math.floor(Math.random()*(wordSet.length-index));
+        element.innerHTML = wordSet[location];
+        wordSet[location] = wordSet[index];
+        wordSet[index] = element.innerHTML;
+    });
 
-	return false;
+    count();
+
+    return false;
+}
+
+function count() {
+    $("#blue_count").text($(":not(.clicked)[data-color=blue]").size());
+    $("#red_count").text($(":not(.clicked)[data-color=red]").size());
 }
 
 function spyMaster(){
-	$("#board").toggleClass("spymaster");
+    $("#board").toggleClass("spymaster");
 }
 
 setup();
