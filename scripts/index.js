4a4a9a8e549b50316e24a05849ad321aaa961294
dcep93 <dan@quizlet.com>
2020-02-15 17:10:08 -0800

    reformat

--- scripts/index.js
+++ scripts/index.js
@@ -1,71 +1,78 @@
-function setup(){
-    for(var i=0;i<8;i++){
-        newWord("red");
-        newWord("blue");
-    }
-    for(var i=0;i<7;i++){
-        newWord("yellow");
-    }
-    newWord("black");
-    newWord().addClass("wildcard");
+function setup() {
+	for (var i = 0; i < 8; i++) {
+		newWord("red");
+		newWord("blue");
+	}
+	for (var i = 0; i < 7; i++) {
+		newWord("yellow");
+	}
+	newWord("black");
+	newWord().addClass("wildcard");
 
+	$(".word").click(function() {
+		$(this).toggleClass("clicked");
+		count();
+	});
 
-    $(".word").click(function(){
-        $(this).toggleClass("clicked");
-        count();
-    });
-
-    if(window.location.href.indexOf('?') !== -1) {
-        $("#undercover_label").show();
-    }
-
+	if (window.location.href.indexOf("?") !== -1) {
+		$("#undercover_label").show();
+	}
 }
 
-function newWord(color){
-    var index = $("#board").children().length;
-    return $("<div/>").attr("data-index",index).addClass("word").attr("data-color",color).append("<div/>").appendTo("#board");
+function newWord(color) {
+	var index = $("#board").children().length;
+	return $("<div/>")
+		.attr("data-index", index)
+		.addClass("word")
+		.attr("data-color", color)
+		.append("<div/>")
+		.appendTo("#board");
 }
 
-function fire(){
-    $("*").removeClass("spymaster clicked");
+function fire() {
+	$("*").removeClass("spymaster clicked");
 
-    //get seed and set the seed for randomizer
-    Math.seedrandom($("#seed").val());
+	//get seed and set the seed for randomizer
+	Math.seedrandom($("#seed").val());
 
-    var wordSet = $("#undercover").prop('checked') ? undercover : data;
+	var wordSet = $("#undercover").prop("checked") ? undercover : data;
 
-    wordSet.sort();
+	wordSet.sort();
 
-    var color = Math.random() > 0.5 ? "red" : "blue";
-    $(".wildcard").attr("data-color",color);
-    $("#team").css({color:color}).text(color.toUpperCase());
+	var color = Math.random() > 0.5 ? "red" : "blue";
+	$(".wildcard").attr("data-color", color);
+	$("#team")
+		.css({ color: color })
+		.text(color.toUpperCase());
 
-    words = $(".word").sort(function(i,j){return i.getAttribute("data-index")-j.getAttribute("data-index")});
+	words = $(".word").sort(function(i, j) {
+		return i.getAttribute("data-index") - j.getAttribute("data-index");
+	});
 
-    while(words.length > 0){
-        $(words.splice(Math.random()*words.length,1)[0]).appendTo("#board");
-    }
+	while (words.length > 0) {
+		$(words.splice(Math.random() * words.length, 1)[0]).appendTo("#board");
+	}
 
-    $(".word *").each(function(index,element){
-        if($("#seed").val()=="ericisgay"){element.innerHTML = Math.random()<0.33 ? "eric" : (Math.random()<0.5 ? "is" : "gay");return;};
-        var location = index+Math.floor(Math.random()*(wordSet.length-index));
-        element.innerHTML = wordSet[location];
-        wordSet[location] = wordSet[index];
-        wordSet[index] = element.innerHTML;
-    });
+	$(".word *").each(function(index, element) {
+		var location =
+			index + Math.floor(Math.random() * (wordSet.length - index));
+		element.innerHTML = wordSet[location];
+		wordSet[location] = wordSet[index];
+		wordSet[index] = element.innerHTML;
+	});
 
-    count();
+	count();
 
-    return false;
+	return false;
 }
 
 function count() {
-    $("#blue_count").text($(":not(.clicked)[data-color=blue]").size());
-    $("#red_count").text($(":not(.clicked)[data-color=red]").size());
+	$("#blue_count").text($(":not(.clicked)[data-color=blue]").size());
+	$("#red_count").text($(":not(.clicked)[data-color=red]").size());
 }
 
-function spyMaster(){
-    $("#board").toggleClass("spymaster");
+function spyMaster() {
+	$("#board").toggleClass("spymaster");
 }
 
 setup();
